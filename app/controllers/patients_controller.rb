a5eece7b561647b1473d8457e62fe4fa4ff75bd8
cami oliva <lamica.olivares@gmail.com>
2024-12-09 12:33:50 -0300

    update view and controller

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -65,6 +65,6 @@ class PatientsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def patient_params
-      params.require(:patient).permit(:name, :age)
+      params.require(:patient).permit(:name, :age, )
     end
 end
